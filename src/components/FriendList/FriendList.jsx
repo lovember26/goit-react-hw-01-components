946e9e9cf5756f584a2191fec1e98f8a15fc9521
Tetiana Prokopchuk <tetyanaprokopchuk@ukr.net>
2023-03-08 12:21:43 +0200

    create

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className="friend-list">
+      {friends.map(friend => {
+        return (
+          <li key={friend.id} className="item">
+            <span className="status">{friend.isOnline}</span>
+            <img
+              className="avatar"
+              src={friend.avatar}
+              alt="User avatar"
+              width="48"
+            />
+            <p className="name">{friend.name}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className="statistics">
+      {title && <h2 className="title">{title}</h2>}
+      <ul className="stat-list">
+        {stats.map(el => {
+          return (
+            <li key={el.id} className="item">
+              <span className="label">{el.label}</span>
+              <span className="percentage">{el.percentage}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
